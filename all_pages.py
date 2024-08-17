a2227221ebe16a6af485f593b01f3de755bf8a3e
Sumanta Kumar Sahu <126848456+sumanta357@users.noreply.github.com>
2024-08-17 05:35:37 +0000

    Pending changes exported from your codespace

---
+++ all_pages.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+#--------------page setup---------------
+about_page = st.page(
+    page="about_me.py",
+    title="About Me",
+    icon=":material/account_circle",
+    default=True,
+)
